ec9d6e1b999d3aa96af16a707bcbd7563670c456
Eugene Sharygin <eush77@gmail.com>
2014-10-09 19:29:05 +0400

    Save common Logger methods in the prototype object
    This is more efficient than creating functions ad-hoc.

--- index.js
+++ index.js
@@ -23,25 +23,28 @@ var symbols = extend({}, logSymbols, colors.reduce(function (symbols, color) {
 }, {}));
 
 
-var setSymbols = function (logger) {
-  Object.keys(symbols).forEach(function (key) {
-    logger[key] = function (message, options) {
-      logger(symbols[key], message, options);
-      return logger;
-    };
-  });
-
-  logger.puts = function () {
-    var putsLogger = logger({ template: '${message}' });
+var loggerPrototype = {
+  puts: function () {
+    var putsLogger = this({ template: '${message}' });
 
     [].forEach.call(arguments, function (message) {
       putsLogger('', message);
     });
 
-    return logger;
-  };
+    return this;
+  }
+};
 
-  return logger;
+
+Object.keys(symbols).forEach(function (key) {
+  loggerPrototype[key] = function (message, options) {
+    return this(symbols[key], message, options);
+  };
+});
+
+
+var makeLogger = function (log) {
+  return extend(log, loggerPrototype);
 };
 
 
@@ -54,7 +57,7 @@ var makeLoggerWithDefaults = function (defaults) {
   defaults = defaults || {};
   var logger;
 
-  return logger = extend(setSymbols(function (marker, message, options) {
+  return logger = extend(makeLogger(function (marker, message, options) {
     if (typeof marker != 'string') {
       // Configure logger to use predefined options for subsequent calls.
       options = marker;
